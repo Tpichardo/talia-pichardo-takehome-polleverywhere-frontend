a2298112cc66ff56c006432bee45645eb2e6e892
Talia Pichardo <taliapichardo@gmail.com>
2024-01-23 00:10:04 -0500

    Create routes and add dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import NavBar from "./components/navbar/NavBar";
+import Home from "./components/home/Home";
+import allRaffles from "./components/raffles/AllRaffles";
+import AddNewRaffleForm from "./components/newRaffle/AddNewRaffleForm";
+
+// import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Routes>
+				<Route
+					path="/"
+					element={Home}
+				/>
+				<Route
+					path="/raffles"
+					element={allRaffles}
+				/>
+				<Route
+					path="/raffles/new"
+					element={AddNewRaffleForm}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
