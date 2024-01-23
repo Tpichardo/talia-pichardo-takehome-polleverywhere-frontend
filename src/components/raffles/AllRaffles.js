946d9a88828c2efabf98da6b60d1997826a0016f
Talia Pichardo <taliapichardo@gmail.com>
2024-01-23 14:07:26 -0500

    Add card component and buttons to display raffles

--- src/components/raffles/AllRaffles.js
+++ src/components/raffles/AllRaffles.js
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { Button, Card } from "react-bootstrap";
+import { format } from "date-fns";
+
 import getAllRaffles from "../../getAllRaffles";
+import "./AllRaffles.css";
 
 const AllRaffles = () => {
 	const { isLoading, isError, data } = useQuery({
@@ -23,11 +28,27 @@ const AllRaffles = () => {
 	const raffle = data;
 
 	return (
-		<div>
-			<h1>All Rafles:</h1>
+		<div className="rafflesContainer">
+			<h1>Raffles</h1>
 			<ul>
 				{raffle?.map((raffle) => {
-					return <li key={raffle.id}>{raffle.raffle_name}</li>;
+					const formattedDate = format(
+						raffle.date_created,
+						"EEEE MMMM d, yyyy 'at' h:mm a"
+					);
+					return (
+						<li key={raffle.id}>
+							<Card className="text-center">
+								<Card.Body>
+									<Card.Title>{raffle.raffle_name}'s Raffle</Card.Title>
+									<Card.Text>{formattedDate}</Card.Text>
+									<Link to="/raffles/new">
+										<Button variant="primary">Join Raffle</Button>
+									</Link>
+								</Card.Body>
+							</Card>
+						</li>
+					);
 				})}
 			</ul>
 		</div>
