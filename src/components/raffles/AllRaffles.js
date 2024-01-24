ba0b31191632f8133385f76d17665ccb799d9743
Talia Pichardo <taliapichardo@gmail.com>
2024-01-24 18:30:46 -0500

    update allRaffles to manage state

--- src/components/raffles/AllRaffles.js
+++ src/components/raffles/AllRaffles.js
@@ -1,16 +1,25 @@
-import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { Button, Card } from "react-bootstrap";
 import { format } from "date-fns";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { Card, Button } from "react-bootstrap";
 
 import getAllRaffles from "../../getAllRaffles";
 import "./AllRaffles.css";
 
 const AllRaffles = () => {
-	const { isLoading, isError, data } = useQuery({
+	const [allRaffles, setAllRaffles] = useState([]);
+
+	const { data, isLoading, isError } = useQuery({
 		queryKey: ["raffles"],
 		queryFn: getAllRaffles,
+		staleTime: Infinity,
 	});
+
+	if (data !== allRaffles) {
+		setAllRaffles(data);
+	}
 
 	if (isLoading) {
 		return (
@@ -25,13 +34,11 @@ const AllRaffles = () => {
 		return <h1>Hmmm, We are having trouble getting the raffles</h1>;
 	}
 
-	const raffle = data;
-
 	return (
 		<div className="rafflesContainer">
 			<h1>Raffles</h1>
 			<ul>
-				{raffle?.map((raffle) => {
+				{allRaffles?.map((raffle) => {
 					const formattedDate = format(
 						raffle.date_created,
 						"EEEE MMMM d, yyyy 'at' h:mm a"
@@ -44,7 +51,7 @@ const AllRaffles = () => {
 								<Card.Body>
 									<Card.Title>{raffle.raffle_name}'s Raffle</Card.Title>
 									<Card.Text>{formattedDate}</Card.Text>
-									<Link to="/raffles/new">
+									<Link to={`/raffles/${raffle.id}`}>
 										<Button variant="primary">Join Raffle</Button>
 									</Link>
 								</Card.Body>
